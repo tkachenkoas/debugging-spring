83d13854ebef36975b3da164a3a3bd435c4925ef
Anton Tkachenko <thachenkoas@gmail.com>
2023-03-18 19:41:40 +0200

    tenant-debugging

--- context-refresh/src/test/java/examples/debuggingspring/refresh/configurations/ConfigurationDefinedComponentsBehaviorTest.java
+++ context-refresh/src/test/java/examples/debuggingspring/refresh/configurations/ConfigurationDefinedComponentsBehaviorTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils.qualifiedBeanOfType;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class Z_ConfigurationDefinedComponentsBehaviorTest extends PropertiesUpdateBaseTest {
+public class ConfigurationDefinedComponentsBehaviorTest extends PropertiesUpdateBaseTest {
 
     @Order(value = 0)
     @Test
